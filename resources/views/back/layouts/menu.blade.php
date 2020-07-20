5685316b131ddc91610d9ad83c8945aac7f1becf
kubik127 <gmagtymow@gmail.com>
2020-07-20 20:09:58 +0300

    Kategori ekleme işlemleri

--- resources/views/back/layouts/menu.blade.php
+++ resources/views/back/layouts/menu.blade.php
@@ -47,19 +47,10 @@
 
       <!-- Nav Item - Utilities Collapse Menu -->
       <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
-          <i class="fas fa-fw fa-wrench"></i>
-          <span>Utilities</span>
+        <a class="nav-link" @if(Request::segment(2)=="kategoriler") style="color:white !important;" @endif href="{{route('admin.category.index')}}">
+          <i @if(Request::segment(2)=="kategoriler") style="color:white !important;" @endif class="fas fa-fw fa-list"></i>
+          <span>Kategoriler</span>
         </a>
-        <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
-          <div class="bg-white py-2 collapse-inner rounded">
-            <h6 class="collapse-header">Custom Utilities:</h6>
-            <a class="collapse-item" href="utilities-color.html">Colors</a>
-            <a class="collapse-item" href="utilities-border.html">Borders</a>
-            <a class="collapse-item" href="utilities-animation.html">Animations</a>
-            <a class="collapse-item" href="utilities-other.html">Other</a>
-          </div>
-        </div>
       </li>
 
       <!-- Divider -->
@@ -72,7 +63,7 @@
 
       <!-- Nav Item - Pages Collapse Menu -->
       <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
+        <a class="nav-link" href="#">
           <i class="fas fa-fw fa-folder"></i>
           <span>Pages</span>
         </a>
--- routes/web.php
+++ routes/web.php
@@ -11,12 +11,17 @@ Route::post('giris','Back\AuthController@loginPost')->name('login.post');
 
 Route::prefix('admin')->name('admin.')->middleware('isAdmin')->group(function(){
 Route::get('panel','Back\Dashboard@index')->name('dashboard');
+// Makale routes
 Route::get('makaleler/silinenler','Back\ArticleController@trashed')->name('trashed.article');
 Route::resource('makaleler','Back\ArticleController');
 Route::get('/switch','Back\ArticleController@switch')->name('switch');
 Route::get('/deletearticle/{id}','Back\ArticleController@delete')->name('delete.article');
 Route::get('/harDeletearticle/{id}','Back\ArticleController@hardDelete')->name('hard.delete.article');
 Route::get('/recoverarticle/{id}','Back\ArticleController@recover')->name('recover.article');
+// Kategori Routes
+Route::get('/kategoriler','Back\CategoryController@index')->name('category.index');
+Route::post('/kategoriler/create','Back\CategoryController@create')->name('category.create');
+Route::get('/kategori/status','Back\CategoryController@switch')->name('category.switch');
 Route::get('cikis','Back\AuthController@logout')->name('logout');
 });
 
